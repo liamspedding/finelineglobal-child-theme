42eb5325a623090f32f221e6559c16c4815136bf
Liam Spedding <l.spedding@tig.uk.net>
2021-07-19 16:20:01 +0100

    Create Technology Portfolio and fix header in page builder

--- functions.php
+++ functions.php
@@ -159,6 +159,7 @@ require get_template_directory() . '/inc/cpt/job-vacancies.php';
 require get_template_directory() . '/inc/cpt/knowledge-base.php';
 require get_template_directory() . '/inc/cpt/timeline.php';
 require get_template_directory() . '/inc/cpt/case-studies.php';
+require get_template_directory() . '/inc/cpt/technology-portfolio.php';
 
 /**
  * Implement the Custom Header feature.
