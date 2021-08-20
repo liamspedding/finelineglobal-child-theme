9ec9432407abd11020a60adad9ce2c17537ad15e
Liam Spedding <l.spedding@tig.uk.net>
2021-08-20 14:23:05 +0100

    Fix homepage tooltips

--- inc/cpt/customise-posts.php
+++ inc/cpt/customise-posts.php
@@ -14,7 +14,6 @@
 //     }
 // }
 
-add_filter( 'manage_posts_columns', 'set_custom_edit_book_columns' );
 function set_custom_edit_book_columns($columns) {
    unset( $columns['title'] );
    unset( $columns['author'] );
@@ -29,15 +28,24 @@ function set_custom_edit_book_columns($columns) {
 
     return $columns;
 }
+add_filter( 'manage_posts_columns', 'set_custom_edit_book_columns' );
 
 // Add the data to the custom columns for the book post type:
-   add_action( 'manage_posts_custom_column' , 'custom_book_column', 10, 2 );
-   function custom_book_column( $column, $post_id ) {
-       switch ( $column ) {
-   
-           case 'post_thumb' :
-               echo the_post_thumbnail( array(200, 200) );
-               break;
-   
-       }
-   }
+function custom_book_column( $column, $post_id ) {
+      switch ( $column ) {
+
+         case 'post_thumb' :
+            echo the_post_thumbnail( array(200, 200) );
+            break;
+
+      }
+}
+add_action( 'manage_posts_custom_column' , 'custom_book_column', 10, 2 );
+
+// Add Menu Order support
+add_action('admin_init', 'mpe_add_portfolio_page_attributes');
+function mpe_add_portfolio_page_attributes(){
+
+    add_post_type_support( 'technology-portfolio', 'page-attributes' );
+
+}
