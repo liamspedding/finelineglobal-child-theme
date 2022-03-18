a8cc1ee975b4ae403bc6eb612bc640583f6666ac
Liam Spedding <l.spedding@tig.uk.net>
2022-03-18 08:49:23 +0000

    Wrap Extra Info in IF statment

--- inc/shortcodes/locations-shortcode.php
+++ inc/shortcodes/locations-shortcode.php
@@ -36,7 +36,7 @@
                            <div class="text-wrapper">
                               <p class="title"><?php echo get_the_title(); ?></p>      
                               <p><?php echo $address; ?></p>
-                              <p><?php echo $extra_info; ?></p>
+                              <p><?php if( !empty($extra_info) ) : echo $extra_info; endif; ?></p>
                               <div class="contact-wrapper">
                                  <?php
                                  
